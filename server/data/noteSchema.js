a1142be2f15dc426a7b35dd8215c117557de8845
Kevin Jolley <serrowpls@gmail.com>
2018-08-13 14:37:46 -0500

    server post/get work

---
+++ server/data/noteSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  body: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+});
+
+// pre-save
+
+noteSchema.pre('save', function(next) {
+  console.log('pre save hook');
+
+  next();
+});
+
+module.exports = mongoose.model('Note', noteSchema);
---
+++ server/server.js
@@ -0,0 +1,59 @@
+// Node
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const passport = require('passport');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+// Local Routes
+
+mongoose.connect('mongodb://localhost:27017/lambda-notes');
+
+// Server
+const server = express();
+
+// Logger
+
+function logger(req, res, next) {
+  console.log('body: ', req.body);
+
+  next();
+}
+
+// Middleware
+server.use(morgan());
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(logger);
+
+// Imports
+
+const Note = require('./data/noteSchema.js');
+
+// Server
+
+server.get('/', function(req, res) {
+  Note.find().then(notes => {
+    res.json(notes);
+  });
+});
+
+server.post('/post', (req, res) => {
+  const note = new Note(req.body);
+
+  note
+    .save()
+    .then(savedNote => {
+      res.send('Note Saved');
+    })
+    .catch(err => {
+      res.status(400).send('Unable to save Note');
+    });
+});
+
+const port = 5000;
+server.listen(port, () =>
+  console.log(`d-(OvO")z up and running on port ${port}`)
+);
