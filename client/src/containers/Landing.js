4bd805d57ca229f8108805e71cef6dfd8ef85d4e
Kevin Jolley <serrowpls@gmail.com>
2018-08-25 23:16:33 -0500

    added failsafe for second login screen

--- client/src/containers/Landing.js
+++ client/src/containers/Landing.js
@@ -7,16 +7,25 @@ import orange from '@material-ui/core/colors/orange';
 class Landing extends Component {
   constructor(props) {
     super(props);
-    this.toggleClass = this.toggleClass.bind(this);
+    this.toggleClass1 = this.toggleClass1.bind(this);
+    this.toggleClass2 = this.toggleClass2.bind(this);
     this.state = {
-      active: false,
+      active1: false,
+      active2: false,
     };
     // this.toggle = this.toggle.bind(this);
   }
 
-  toggleClass() {
-    let currentState = this.state.active;
-    this.setState({ active: !currentState });
+  toggleClass1() {
+    let currentState = this.state.active1;
+    this.setState({ active1: !currentState });
+    this.setState({ active2: false });
+  }
+
+  toggleClass2() {
+    let currentState = this.state.active2;
+    this.setState({ active2: !currentState });
+    this.setState({ active1: false });
   }
 
   render() {
@@ -36,8 +45,15 @@ class Landing extends Component {
       <Column width="100%" height="100%" justify="space-evenly">
         <LandingRow>
           <h1> Lots of Notes </h1>
-          <LandingRender className={this.state.active ? 'display' : 'none'}>
+          <LandingRender className={this.state.active1 ? 'display' : 'none'}>
             <p> Let's get you signed up (OvO") </p>
+            <SignInput width="60%" type="text" name="username" required />
+            <SignInput width="60%" type="password" name="password" required />
+            <SignInput inputbutton type="submit" />
+          </LandingRender>
+
+          <LandingRender className={this.state.active2 ? 'display' : 'none'}>
+            <p> Welcome Back (OvO") </p>
             <SignInput width="60%" type="text" name="username" required />
             <SignInput width="60%" type="password" name="password" required />
             <SignInput inputbutton type="submit" />
@@ -50,7 +66,7 @@ class Landing extends Component {
               color="primary"
               size="large"
               className="button_style"
-              onClick={this.toggleClass}
+              onClick={this.toggleClass1}
             >
               Create an Account
             </Button>
@@ -59,6 +75,7 @@ class Landing extends Component {
               color="primary"
               size="large"
               className="button_style"
+              onClick={this.toggleClass2}
             >
               Log-In
             </Button>
